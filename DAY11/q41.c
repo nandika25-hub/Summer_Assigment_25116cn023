040c2a3d6d5205ba2fd9cef99ec4dc288d80b204
nandika25-hub <nandikamishra25@gmail.com>
2026-06-11 20:00:32 +0530

    Summer Assignment

---
+++ DAY11/q41.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    printf("%d", sum(10, 20));
+    return 0;
+}
---
+++ DAY11/q43.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int max(int a, int b)
+{
+    if(a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    printf("%d", max(10, 20));
+    return 0;
+}
