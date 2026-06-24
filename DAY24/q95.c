b2196962e3172cc809fe918885229406fd4d91f4
nandika25-hub <nandikamishra25@gmail.com>
2026-06-24 20:29:12 +0530

    Summer Assigment

---
+++ DAY24/q95.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200], longest[50];
+    int maxLen = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    char *word = strtok(str, " \n");
+
+    while(word != NULL) {
+        int len = strlen(word);
+
+        if(len > maxLen) {
+            maxLen = len;
+            strcpy(longest, word);
+        }
+
+        word = strtok(NULL, " \n");
+    }
+
+    printf("Longest Word: %s\n", longest);
+    printf("Length: %d\n", maxLen);
+
+    return 0;
+}
